bb3d60c0e0b2248ea5e70174169134865f4692ca
Mounir El-Abbas <eamounir1@gmail.com>
2021-12-15 15:35:14 -0500

    Boilerplate for app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./scss/main.scss";
+import "./styles/css/main.css";
 import App from "./App";
 import AppProvider from "./context";
 
